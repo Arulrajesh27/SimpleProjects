9b31397cce7367e4d3a83d4de525600c1442f003
Arulrajesh27 <rajesh.arul11@gmail.com>
2023-04-05 17:04:41 +0530

    First time push

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,18 @@
+package org.company;
+
+public class CompanyInfo {
+	private void companyName() {
+		
+System.out.println("TCS");
+	}private void companyId() {
+		
+System.out.println("TCS 1000000");
+	}private void companyAddress() {
+		
+System.out.println("Chennai");
+	}
+	public static void main(String[] args) {
+		CompanyInfo o=new CompanyInfo();
+		o.companyName();o.companyId();o.companyAddress();
+	}
+}
